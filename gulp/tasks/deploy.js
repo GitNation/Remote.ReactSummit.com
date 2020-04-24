3d38beccec3b5e88537c0066c2187b21fc4dddbe
Oleg Proskurin <regx@usul.su>
2020-04-24 19:05:45 +0300

    revert testing changes

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -17,28 +17,23 @@ gulp.task('deploy', function() {
     parallel: 2,
   });
 
-  gulp.src(['./build/css/appe2c31e.css']).pipe(conn.dest('/css'));
-  gulp.src(['./build/js/app5beb41.js']).pipe(conn.dest('/js'));
-  gulp.src(['./build/index.html']).pipe(conn.dest(remotePath));
-  gulp.src(['./build/test.html']).pipe(conn.dest(remotePath));
+  // Always deploy HTML
+  gulp.src(['./build/*.*']).pipe(conn.dest(remotePath));
 
-  // // Always deploy HTML
-  // gulp.src(['./build/*.*']).pipe(conn.dest(remotePath));
+  // Always deploy CSS
+  gulp.src(['./build/css/**/*.*']).pipe(conn.dest('/css'));
 
-  // // Always deploy CSS
-  // gulp.src(['./build/css/**/*.*']).pipe(conn.dest('/css'));
+  // Always deploy JS
+  gulp.src(['./build/js/**/*.*']).pipe(conn.dest('/js'));
 
-  // // Always deploy JS
-  // gulp.src(['./build/js/**/*.*']).pipe(conn.dest('/js'));
-
-  // // Compare size of other files before deploy
-  // gulp
-  //   .src([
-  //     './build/**/*.*',
-  //     '!./build/*.*',
-  //     '!./build/css/**/*.*',
-  //     '!./build/js/**/*.*',
-  //   ])
-  //   .pipe(conn.differentSize(remotePath))
-  //   .pipe(conn.dest(remotePath));
+  // Compare size of other files before deploy
+  gulp
+    .src([
+      './build/**/*.*',
+      '!./build/*.*',
+      '!./build/css/**/*.*',
+      '!./build/js/**/*.*',
+    ])
+    .pipe(conn.differentSize(remotePath))
+    .pipe(conn.dest(remotePath));
 });
--- src/js/app.js
+++ src/js/app.js
@@ -42,16 +42,13 @@ fancypop();
 
 highlightContent({ contentTypeMap });
 
-window.selectGeneralTrack = () =>
-  document.querySelector('a.schedule__tab-link[data-tab="1"]').click();
-window.selectNativeTrack = () =>
-  document.querySelector('a.schedule__tab-link[data-tab="2"]').click();
+window.selectGeneralTrack = () => document.querySelector('a.schedule__tab-link[data-tab="1"]').click();
+window.selectNativeTrack = () => document.querySelector('a.schedule__tab-link[data-tab="2"]').click();
 
 if (location.hash === '#alt-track') {
   try {
-    document
-      .querySelector('.live-programm__tab.js-tab-link[data-tab="2"]')
-      .click();
-  } catch (err) {}
+    document.querySelector('.live-programm__tab.js-tab-link[data-tab="2"]').click();
+  } catch (err) {
+
+  }
 }
-console.log('testing new hash');
